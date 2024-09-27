689bbaa663abdd937433a711e5d9deea7281851c
SF <artistsilen@gmail.com>
2024-09-27 16:36:28 +0100

    fixed perm within replica

--- postgres-replica/replica_init.sh
+++ postgres-replica/replica_init.sh
@@ -10,6 +10,9 @@ until pg_isready -h "$PG_PRIMARY_HOST" -p "$PG_PRIMARY_PORT"; do
 done
 
 mkdir -p /var/lib/postgresql/pg_backup
+#chown postgres:postgres /var/lib/postgresql/pg_backup
+chmod 700 -R /var/lib/postgresql/pg_backup
+
 pg_basebackup -h "$PG_PRIMARY_HOST" -D /var/lib/postgresql/pg_backup -U "$REPLICATOR_USER" -v -P -Xs
 
 cp /tmp/postgresql.conf $PGDATA/postgresql.conf
@@ -20,4 +23,4 @@ echo "primary_conninfo = 'host=$PG_PRIMARY_HOST port=$PG_PRIMARY_PORT user=$REPL
 touch /var/lib/postgresql/data/standby.signal
 
 chown postgres:postgres $PGDATA/standby.signal
-chmod 700 $PGDATA/standby.signal
+chmod u=rwx $PGDATA/standby.signal
