789c23f3a2452e505746375f27d0d61596a53595
SF <artistsilen@gmail.com>
2024-09-29 14:18:01 +0100

    v2

--- postgres-replica/replica_init.sh
+++ postgres-replica/replica_init.sh
@@ -10,17 +10,12 @@ until pg_isready -h "$PG_PRIMARY_HOST" -p "$PG_PRIMARY_PORT"; do
 done
 
 mkdir -p /var/lib/postgresql/pg_backup
-#chown postgres:postgres /var/lib/postgresql/pg_backup
+chown postgres:postgres /var/lib/postgresql/pg_backup
 chmod 700 -R /var/lib/postgresql/pg_backup
 
-pg_basebackup -h "$PG_PRIMARY_HOST" -D /var/lib/postgresql/pg_backup -U "$REPLICATOR_USER" -v -P -Xs
+pg_basebackup -h "$PG_PRIMARY_HOST" -D /var/lib/postgresql/pg_backup -U "$REPLICATOR_USER" -v -P -R -Xs
 
+cp -r /var/lib/postgresql/pg_backup /var/lib/postgresql/data
 cp /tmp/postgresql.conf $PGDATA/postgresql.conf
 cp /tmp/pg_hba.conf $PGDATA/pg_hba.conf
-echo "primary_conninfo = 'host=$PG_PRIMARY_HOST port=$PG_PRIMARY_PORT user=$REPLICATOR_USER password=$REPLICATOR_PASSWORD'" >> /var/lib/postgresql/data/postgresql.conf
-
-
-touch /var/lib/postgresql/data/standby.signal
-
-chown postgres:postgres $PGDATA/standby.signal
-chmod u=rwx $PGDATA/standby.signal
+echo "primary_conninfo = 'host=$PG_PRIMARY_HOST port=$PG_PRIMARY_PORT user=$REPLICATOR_USER password=$REPLICATOR_PASSWORD'" >> $PGDATA/postgresql.conf
